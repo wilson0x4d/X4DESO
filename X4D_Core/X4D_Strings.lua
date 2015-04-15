13cd62e7d059607bf24844f32ccb2cc0258cbecd
Shaun Wilson <mrshaunwilson@msn.com>
2015-04-15 01:10:40 -0700

    [core] new 'X4D_Strings.lua'

---
+++ X4D_Core/X4D_Strings.lua
@@ -0,0 +1,16 @@
+function string:Split(delimiter)
+    local input = '' .. self;
+    local result = {}
+    for match in (input..delimiter):gmatch("(.-)"..delimiter) do
+		local s = tostring(match)
+		if (s:len() > 0) then
+			table.insert(result, match)
+		end
+    end
+    return result
+end
+
+function string:EndsWith(v)
+    local input = '' .. self;
+    return v=='' or string.sub(input,-string.len(v))==v
+end
