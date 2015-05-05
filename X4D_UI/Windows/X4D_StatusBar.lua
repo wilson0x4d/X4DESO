3a3d6d07c03debafa49121216827a8a00ddd2d59
Shaun Wilson <mrshaunwilson@msn.com>
2015-05-05 03:10:28 -0700

    [ui/statusbar] add 'clock/time' to performance panel

--- X4D_UI/Windows/X4D_StatusBar.lua
+++ X4D_UI/Windows/X4D_StatusBar.lua
@@ -19,14 +19,18 @@ local X4D_StatusBarPanel = {}
 
 --- 
 --- param onUpdateStatusCallback == no args, state management onus of caller
-function X4D_StatusBarPanel:New(name, onUpdateStatusCallback)
+function X4D_StatusBarPanel:New(name, onUpdateStatusCallback, updateFrequency)
     -- TODO: if _statusBarWinow == nil then throw InvalidOperation
+    if (updateFrequency == nil) then
+        updateFrequency = 3
+    end
     local panel = {
         OnUpdateStatus = onUpdateStatusCallback,
         Label = CreateControl(name, _statusBarWindow, CT_LABEL),
         Order = 0,
         Width = 0,
         Offset = 0,
+        UpdateFrequency = updateFrequency,
         AnchorPoint = BOTTOMRIGHT, -- default layout from bottomright, will also layout from bottomleft
         SetText = function (self, text)
             _private_label:SetText(text)
@@ -55,26 +59,30 @@ end
 
 --endregion
 
-function X4D_StatusBar:CreatePanel(name, onUpdateStatusCallback)
+function X4D_StatusBar:CreatePanel(name, onUpdateStatusCallback, updateFrequency)
     self:Initialize()
-    return X4D_StatusBarPanel:New(name, onUpdateStatusCallback)
+    return X4D_StatusBarPanel:New(name, onUpdateStatusCallback, updateFrequency)
 end
 
-local function UpdateStatusBarPanels()
+local function UpdateStatusBarPanels(tickCount)
     local leftOffset = 0
     local rightOffset = 0
+    -- TODO: sort panels by panel.Order first
     for panelName,statusBarPanel in pairs(X4D_StatusBar.Panels) do
         if (statusBarPanel.OnUpdateStatus ~= nil) then
             if (statusBarPanel.AnchorPoint == BOTTOMRIGHT) then
                 statusBarPanel.Offset = rightOffset
-                statusBarPanel.OnUpdateStatus()
+                if ((tickCount <= 3) or ((tickCount % statusBarPanel.UpdateFrequency) == 0)) then
+                    statusBarPanel.OnUpdateStatus()
+                end
                 rightOffset = rightOffset + statusBarPanel.Width
             else
                 statusBarPanel.Offset = leftOffset
-                statusBarPanel.OnUpdateStatus()
+                if ((tickCount <= 3) or ((tickCount % statusBarPanel.UpdateFrequency) == 0)) then
+                    statusBarPanel.OnUpdateStatus()
+                end
                 leftOffset = leftOffset + statusBarPanel.Width
             end
-            -- TODO: use panel.Order instead of natural order
         end
     end            
 end
@@ -88,6 +96,48 @@ local _latencyMeterIcons = {
     [1] = X4D.Icons:CreateString("/esoui/art/campaign/campaignbrowser_medpop.dds", 16, 16),
     [2] = X4D.Icons:CreateString("/esoui/art/campaign/campaignbrowser_lowpop.dds", 16, 16),
 }
+
+local function GetTimestampPrefix(color)
+    if (X4D.Chat == nil or X4D.Chat.Settings == nil) then
+        return ""
+    end
+    local timestampOption = X4D.Chat.Settings:Get('TimestampOption')
+	if (timestampOption == "Disabled") then
+		return color
+	end
+
+	local timeString = GetTimeString()
+
+	if (timestampOption == "12 Hour Format") then
+		local hour = timeString:gmatch("(%d%d).%d%d.%d%d")()
+		if (tonumber(hour) > 12) then
+			hour = tostring(tonumber(hour) - 12)
+			if (hour:len() == 1) then
+				hour = "0" .. hour
+			end
+			if (X4D.Chat.Settings:Get('RemoveSeconds')) then
+				timeString = timeString:gsub("%d%d.(%d%d).%d%d", hour .. ":%1 PM")
+			else
+				timeString = timeString:gsub("%d%d.(%d%d).(%d%d)", hour .. ":%1:%2 PM")
+			end
+		else
+			if (hour == '00') then
+				hour = '12'
+			end
+			if (X4D.Chat.Settings:Get('RemoveSeconds')) then
+				timeString = timeString:gsub("%d%d.(%d%d).%d%d", hour .. ":%1 AM")
+			else
+				timeString = timeString:gsub("%d%d.(%d%d).(%d%d)", hour .. ":%1:%2 AM")
+			end
+		end
+    elseif (X4D.Chat.Settings:Get('RemoveSeconds')) then
+		timeString = timeString:gsub("(%d%d).(%d%d).%d%d", "%1:%2")
+	end
+
+	local highlightColor = X4D.Colors:DeriveHighlight(color)
+	return color .. "  [" .. highlightColor .. timeString .. color .. "] "
+end
+
 
 local function UpdatePerformancePanel()
     local framerate = GetFramerate()
@@ -118,12 +168,12 @@ local function UpdatePerformancePanel()
         latencyColor = X4D.Colors.Yellow
     end
     local latencyString = latencyColor .. "PING: " .. latency --.. _latencyMeterIcons[latencyLevel]
-    local text = "   " .. framerateString .. "   " .. latencyString
+    local text = GetTimestampPrefix(X4D.Colors.Gray) .. "   " .. framerateString .. "   " .. latencyString
     _performancePanel:SetText(text)
 end
 
 local function CreateBuiltInPerformancePanel()
-    _performancePanel = X4D.UI.StatusBar:CreatePanel("X4D_UI_PerformancePanel", UpdatePerformancePanel)
+    _performancePanel = X4D.UI.StatusBar:CreatePanel("X4D_UI_PerformancePanel", UpdatePerformancePanel, 1)
     _performancePanel.AnchorPoint = BOTTOMLEFT
 end
 
@@ -156,13 +206,11 @@ function X4D_StatusBar:Initialize()
         --ZO_AlphaAnimation
 
         CreateBuiltInPerformancePanel()
-        UpdateStatusBarPanels()
+        UpdateStatusBarPanels(0)
         local statusBarUpdateTimer = X4D.Async:CreateTimer(function (timer, state)
             --X4D.Log:Verbose{"X4D_StatusBar", "Updating Status Bar Panels"}
             state.Ticks = state.Ticks + 1
-            if (state.Ticks % 3 == 0) then
-                UpdateStatusBarPanels()
-            end
+            UpdateStatusBarPanels(state.Ticks)
         end, 1000, { Ticks = 0 }) 
         statusBarUpdateTimer:Start()
     end
