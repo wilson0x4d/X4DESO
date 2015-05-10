bbb30e329e578928ac4436ac8d87436fadac78c8
Shaun Wilson <mrshaunwilson@msn.com>
2015-05-10 03:13:49 -0700

    [ui] add an observable for working with scene changes

--- X4D_UI/X4D_UI.lua
+++ X4D_UI/X4D_UI.lua
@@ -8,7 +8,20 @@ X4D.UI = X4D_UI
 X4D_UI.NAME = "X4D_UI"
 X4D_UI.VERSION = "1.0"
 
+X4D.UI.SceneManager = SCENE_MANAGER
+X4D.UI.CurrentScene = X4D.Observables(nil)
+
 function X4D_UI:View(name)
     
 end
 
+X4D.UI.SceneManager:RegisterCallback("SceneStateChanged", function(scene, oldState, newState)
+    if ((newState == SCENE_HIDING) or (newState == SCENE_HIDDEN)) then
+        X4D.Log:Verbose({"SceneStateChanged->Shown", scene:GetName()}, "UI")
+    elseif (scene == nil) then
+        X4D.Log:Verbose({"scene==nil"}, "UI")
+    else
+        X4D.Log:Verbose({"SceneStateChanged->Shown", scene:GetName()}, "UI")
+        X4D.UI.CurrentScene(scene)
+    end
+end)
