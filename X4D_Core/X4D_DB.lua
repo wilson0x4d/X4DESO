97ff7f6f02e1ef6449483c8db5613240814bbfe5
wilson0x4d <mrshaunwilson@msn.com>
2016-10-15 02:37:17 -0700

    [db] fix bug that was preventing databases from persisting to savedvars

--- X4D_Core/X4D_DB.lua
+++ X4D_Core/X4D_DB.lua
@@ -21,41 +21,49 @@ end
 local _databases = nil
 
 -- NOTE: not valid to call except during addon loaded events, unless the input is a Lua table object, in which case it can be called at any time
--- "non-persistent" databases can be created by providing a reference to a Lua "table", X4D_DB delegates access to the table
--- "persistent" databases are opened when a database name (string) is provided
+-- "persistent" databases are opened when a database name (string) is provided, this can only be done successfully during addon loaded event
+-- "transient" databases can be created AT ANY TIME by providing a reference to a Lua "table", X4D_DB delegates access to the table
 function X4D_DB:Open(database, version)
+--	X4D.Log:Debug{"X4D_DB:Open", database, version}
     if (database == nil) then
         database = { }
     end
-    if (type(database) ~= "table") then
+    local databaseName = database -- yes, assumes is a string, or other valid key, but not a table object
+    if (type(databaseName) == "table") then
+		databaseName = "TRANSIENT" .. GetGameTimeMilliseconds()
+	else
         if (version == nil) then
             version = 0
         end
+		version = X4D_DB_VERSION + version
         if (_databases == nil) then
             _databases = X4D.InternalSettings:Get("X4DB")
             if (_databases == nil) then
-                _databases = {}
+				X4D.Log:Verbose{"X4D_DB:Open", "Creating Missing SavedVar"}
+                _databases = { }
                 X4D.InternalSettings:Set("X4DB", _databases)
             end
         end
-        local databaseName = database -- yes, assumes is a string, or other valid key, but not a table object
+		-- NOTE: the following line is useful for confirming (1) that savedvars are saving and (2) that the right number of persistent databases are being opened
+		-- _databases["_seq"] = (tonumber(_databases["_seq"]) or 1) + 1
         database = _databases[databaseName]
-        if (database == nil or (version ~= nil and (database._version == nil or database._version < (X4D_DB_VERSION + version)))) then
+        if (database == nil) then
+			X4D.Log:Verbose{"X4D_DB:Open", "Database Not Found", databaseName, version}
             database = {
-                _version = X4D_DB_VERSION + version
+                _version = version
             }
-            _databases[database] = database
+            _databases[databaseName] = database
+		elseif (database._version == nil or database._version < version) then
+			X4D.Log:Verbose{"X4D_DB:Open", "Bad/Missing Version", databaseName, version}
         end
         database._name = databaseName
-    end
-    if (database._version == nil and version ~= nil) then
-        database._version = X4D_DB_VERSION + version
+		X4D.InternalSettings:Set("X4DB", _databases)
     end
     if (database._count == nil or database._count == 0) then
         database._count = countof(database)
     end
     if (database._name == nil) then
-        database._name = "TRANSIENT" .. GetGameTimeMilliseconds()
+        database._name = databaseName
     end
 	local proto = {
 		_table = database
@@ -65,6 +73,7 @@ function X4D_DB:Open(database, version)
 end
 
 function X4D_DB:Find(key) -- most efficient way to look up an item is if you already have its key
+	X4D.Log:Debug{"X4D_DB:Find", self._table._name, key}
     return self._table[key]
 end
 
@@ -91,19 +100,22 @@ function X4D_DB:Add(key, value)
         value = key
         key = value.Id or value.Key or value.id or value.key or value.ID
     end
+	X4D.Log:Verbose{"X4D_DB:Add", self._table._name, key}
     self._table[key] = value
     self._table._count = self._table._count + 1
     return value, key
 end
 
 function X4D_DB:Remove(key)
+	X4D.Log:Debug{"X4D_DB:Remove", self._table._name, key}
     self._table[key] = nil
     self._table._count = self._table._count - 1
 end
 
 function X4D_DB:Count()
+	X4D.Log:Debug{"X4D_DB:Count", self._table._name}
     -- NOTE: do not perform direct insertions/removals against _table, or you skew _count
-    return self._table._count --countof(self._table)
+    return self._table._count or countof(self._table)
 end
 
 -- discouraged, potential memory hog on very large data sets, may be cheaper to pay for a closure and use :ForEach unless you *really* need to perform a translation (cleaning up data between DB versions, e.g. removing/renaming properties from existing entities)
@@ -117,7 +129,7 @@ function X4D_DB:Select(builder)
     return results
 end
 
--- discouraged, memory hog. use :ForEach and :FirstOrDefault instead.
+-- discouraged, memory hog. use :ForEach and :FirstOrDefault instead. this exists for very specific cases where you need a long-lived copy of results anyway, this is BAD for transient data/operations.
 function X4D_DB:Where(predicate, silence)
     if (silence ~= true) then
         -- warning: this method creates a new table even when returning no results, it is a memory hog when used to query large datasets (use :ForEach or :FirstOrDefault instead.)
@@ -153,7 +165,8 @@ SLASH_COMMANDS["/x4db"] = function (parameters, other)
                         local databaseVersion = database._table._version
                         database._table = {
                             _version = databaseVersion,
-                            _name = databaseName
+                            _name = databaseName,
+							_count = 0
                         }
                         X4D.Log:Information("Reset of '" .. databaseName .. "' complete.")
                         return
@@ -166,7 +179,7 @@ SLASH_COMMANDS["/x4db"] = function (parameters, other)
         --endregion
         --region /x4db count [X4D_Items.DB]
         if (parameters:StartsWith("count")) then
-            if (#args > 1) then
+            if (#args == 2) then
                 local databaseName = args[2]
                 if (databaseName ~= nil) then
                     local table = _databases[databaseName]
@@ -177,10 +190,10 @@ SLASH_COMMANDS["/x4db"] = function (parameters, other)
                 end
             else
                 for _,table in pairs(_databases) do
-                    if (table ~= nil) then
+                    if (table ~= nil and type(table) == "table") then
                         local databaseName = table._name
                         local database = X4D_DB:Create(table)
-                        X4D.Log:Information(databaseName .. " contains " .. database:Count() .. " entities.")
+                        X4D.Log:Information((databaseName or "(nil)") .. " contains " .. database:Count() .. " entities.")
                     end
                 end
            end
