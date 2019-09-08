wilson0x4d/X4DESO

commit f3ca492d5f746b1d3c569570f6f14c53cc196247

[async] fix bug where stopped timer still executes

X4D_Core/X4D_Async.lua
@@ -10,6 +10,7 @@ local _timers = {}
 X4D_Async.ActiveTimers = X4D.DB:Open(_timers)
 
 local X4D_Timer = {}
+local DEFAULT_TIMER_INTERVAL = 1000
 
 --- <summary>
 --- <para>Create a new instance of X4D_Timer</para>
@@ -33,7 +34,7 @@ function X4D_Timer:New(callback, interval, state, name)
 --        _memory = 0, -- for diagnostic purposes only, requires retuning of GC to be of any value, and so commented out for Release
 		_enabled = false,
 		_callback = callback or (function(L_timer) self:Stop() end),
-		_interval = interval or 1000,
+		_interval = interval or DEFAULT_TIMER_INTERVAL,
 		_state = state or {},
 	}
 	setmetatable(proto, { __index = X4D_Timer })
@@ -44,8 +45,12 @@ function X4D_Timer:IsEnabled()
 	return self._enabled
 end
 
-function X4D_Timer:Elapsed()
---    local memory = collectgarbage("count") -- TODO: only perform this count when debug mode has been set
+function X4D_Timer:Elapsed(expectedId)	
+	if (self._id ~= expectedId or not self._enabled) then
+		--X4D.Log:Debug("Timer'"..expectedId.."' was intercepted and cancelled.")
+		return
+	end
+
     self._timestamp = GetGameTimeMilliseconds()
 	if (not self._callback) then
 		self:Stop()
@@ -56,19 +61,35 @@ function X4D_Timer:Elapsed()
 		X4D.Log:Error(err, self.Name)
 		return
 	end
-	if (self._enabled) then
-	    zo_callLater(function() self:Elapsed() end, self._interval)
+	if (self._enabled) then		
+		zo_callLater(function()
+			self:Elapsed(expectedId)
+		end, self._interval)
 	end
 --    memory = (collectgarbage("count") - memory)
 --    if (memory >= 100 or memory <= -100) then
 --        X4D.Log:Debug(self.Name .. " memory delta: " .. (memory * 1024))
 --    end
 --    self._memory = memory
+
+	if (self._id ~= expectedId) then
+		--X4D.Log:Debug("Timer'"..expectedId.."' was NOT intercepted/cancelled, and may have overlapped with a later event.")
+		return
+	end
+
 end
 
 -- "state" is passed into timer callback
 -- "interval" is optional, and can be used to change the timer interval during execution
 function X4D_Timer:Start(interval, state, name)
+	if (self._enabled or self._id ~= nil) then
+		-- attempt cancellation of enabled/active task
+		self:Stop()
+	end
+	-- schedule new
+	local timerId = _nextTimerId
+	_nextTimerId = _nextTimerId + 1
+	self._id = timerId
     if (name ~= nil) then
         self.Name = name
     end
@@ -78,18 +99,25 @@ function X4D_Timer:Start(interval, state, name)
 	if (interval ~= nil) then
 		self._interval = interval
 	end
+	if (self._interval == nil or self._interval <= 0) then
+		-- NOTE: enforcing a sane default
+		self._interval = DEFAULT_TIMER_INTERVAL; 
+	end
 	if (self._enabled) then
 		return
 	end
 	self._enabled = true
-    X4D_Async.ActiveTimers:Add(self._id, self)
-	zo_callLater(function() self:Elapsed() end, self._interval)
+	X4D_Async.ActiveTimers:Add(timerId, self)
+	zo_callLater(function() self:Elapsed(timerId) end, self._interval)
     return self
 end
 
 function X4D_Timer:Stop()
-    X4D_Async.ActiveTimers:Remove(self._id)
-	self._enabled = false
+	if (self._id ~= nil) then
+		X4D_Async.ActiveTimers:Remove(self._id)
+		self._enabled = false
+		self._id = nil
+	end
     return self
 end
 

commit df51286f72493e736c2561f60422edb62e08c75d

[async] fix bug introduced with f3ca492 where timers would not restart

X4D_Core/X4D_Async.lua
@@ -60,7 +60,16 @@ function X4D_Timer:Elapsed(expectedId)
 		X4D.Log:Error(err, self.Name)
 		return
 	end
-	if (self._enabled) then		
+	if (self._enabled) then
+		if (self._id == nil) then
+			-- NOTE: in this case a timer callback has set the timer BACK into an enabled
+			--		 state AFTER calling :Stop() -- this is the ONLY scenario where we
+			--		 restore the timer id on behalf of the caller. the only scenario where
+			--		 _id is nil SHOULD be after a call to :Stop() thus we check for it here
+			--		 before performing state reversals
+			self._id = expectedId
+			X4D_Async.ActiveTimers:Add(self._id, self)
+		end
 		zo_callLater(function()
 			self:Elapsed(expectedId)
 		end, self._interval)